78c0f25c88a7442d322786b377be40ae28a8313f
choiruladamm <choiruladamm@gmail.com>
2023-07-12 21:25:18 +0700

    Handling Not Found Errors

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+
 import Root from "./routes/Root";
+import ErrorPage from "./ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root />
+    element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
